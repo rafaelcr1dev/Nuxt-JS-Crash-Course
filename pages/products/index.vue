4e68deca0142de5d8b2b91f92ba2126dbeba88b7
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-10-17 16:47:14 -0300

    initial

---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="container">
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo.slice(0,1)"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardsSections"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardsSections: smallCardSections
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
